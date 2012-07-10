d364338dcf6e26a58f0bac0fe71d667ef34c7378
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2012-07-10 03:23:24 -0400

    Remove trailing whitespace.

--- sassc.c
+++ sassc.c
@@ -2,20 +2,20 @@
 #include "libsass/sass_interface.h"
 
 int main(int argc, char** argv)
-{	
+{
 	if (argc < 2) {
 		printf("Usage: sassc [INPUT FILE]\n");
 		return 0;
 	}
-	
+
 	struct sass_file_context* ctx = sass_new_file_context();
 	ctx->options.include_paths = "";
 	ctx->options.image_path = "images";
 	ctx->options.output_style = SASS_STYLE_NESTED;
 	ctx->input_path = argv[1];
-		
+
 	sass_compile_file(ctx);
-	
+
 	if (ctx->error_status) {
 		if (ctx->error_message)
 			printf("%s", ctx->error_message);
@@ -28,7 +28,7 @@ int main(int argc, char** argv)
 	else {
 		printf("Unknown internal error.\n");
 	}
-	
+
 	sass_free_file_context(ctx);
 	return 0;
 }
