16771e5f24eeb979f32c4d4790b04d85a2ec92fb
Craig Barnes <cr@igbarn.es>
2012-11-30 20:59:20 +0000

    Completely restructure sassc.c and add support for reading from stdin

--- sassc.c
+++ sassc.c
@@ -1,82 +1,132 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <getopt.h>
 #include "libsass/sass_interface.h"
 
-int main(int argc, char** argv)
-{
-	struct sass_file_context* ctx;
-	char *include_paths = "";
-	char *filename = "";
-	int style = SASS_STYLE_NESTED;
-	int comments = 0;
-	int ret;
-	int c;
+#define BUFSIZE 512
 
-	while ((c = getopt(argc, argv, "lt:I:")) != -1) {
-		switch (c) {
-		case 'I':
-			include_paths = optarg;
-			break;
-		case 't':
-			if (strcmp(optarg, "compressed") == 0) {
-				style = SASS_STYLE_COMPRESSED;
-			} else if (strcmp(optarg, "compact") == 0) {
-				style = SASS_STYLE_COMPACT;
-			} else if (strcmp(optarg, "expanded") == 0) {
-				style = SASS_STYLE_EXPANDED;
-			} else if (strcmp(optarg, "nested") == 0) {
-				style = SASS_STYLE_NESTED;
-			} else {
-				fprintf(stderr, "Invalid argument for -t flag: '%s'\n", optarg);
-				/* No abort here, just use the default and continue */
-			}
-			break;
-		case 'l':
-			comments = 1;
-			break;
-		case '?':
-			/* Unrecognized flag or missing an expected value */
-			/* getopt should produce it's own error message for this case */
-			return 1;
-		default:
-			fprintf(stderr, "Unknown error while processing arguments\n");
-			return 2;
-		}
-	}
+int output(int error_status, char* error_message, char* output_string) {
+    if (error_status) {
+        if (error_message) {
+            fprintf(stderr, "%s", error_message);
+        } else {
+            fprintf(stderr, "An error occured; no error message available.\n");
+        }
+        return 1;
+    } else if (output_string) {
+        printf("%s", output_string);
+        return 0;
+    } else {
+        fprintf(stderr, "Unknown internal error.\n");
+        return 2;
+    }
+}
 
-	if (optind < argc) {
-		filename = argv[optind];
-	} else {
-		fprintf(stderr, "Usage: sassc [OPTION]... FILE\n");
-		return 1;
-	}
+int compile_stdin(struct sass_options options) {
+    int ret;
+    struct sass_context* ctx;
+    char buffer[BUFSIZE];
+    size_t size = 1;
+    char *source_string = malloc(sizeof(char) * BUFSIZE);
 
-	ctx = sass_new_file_context();
-	ctx->options.include_paths = include_paths;
-	ctx->options.image_path = "images";
-	ctx->options.output_style = style;
-	ctx->options.source_comments = comments;
-	ctx->input_path = filename;
+    if(source_string == NULL) {
+        perror("Allocation failed");
+        exit(1);
+    }
 
-	sass_compile_file(ctx);
+    source_string[0] = '\0';
 
-	if (ctx->error_status) {
-		if (ctx->error_message)
-			fprintf(stderr, "%s", ctx->error_message);
-		else
-			fprintf(stderr, "An error occured; no error message available.\n");
-		ret = 1;
-	}
-	else if (ctx->output_string) {
-		printf("%s", ctx->output_string);
-		ret = 0;
-	}
-	else {
-		fprintf(stderr, "Unknown internal error.\n");
-		ret = 2;
-	}
+    while(fgets(buffer, BUFSIZE, stdin)) {
+        char *old = source_string;
+        size += strlen(buffer);
+        source_string = realloc(source_string, size);
+        if(source_string == NULL) {
+            perror("Reallocation failed");
+            free(old);
+            exit(2);
+        }
+        strcat(source_string, buffer);
+    }
 
-	sass_free_file_context(ctx);
-	return ret;
+    if(ferror(stdin)) {
+        free(source_string);
+        perror("Error reading standard input");
+        exit(2);
+    }
+
+    ctx = sass_new_context();
+    ctx->options = options;
+    ctx->source_string = source_string;
+    sass_compile(ctx);
+    ret = output(ctx->error_status, ctx->error_message, ctx->output_string);
+
+    sass_free_context(ctx);
+    free(source_string);
+    return ret;
+}
+
+int compile_file(struct sass_options options, char* input_path) {
+    int ret;
+    struct sass_file_context* ctx = sass_new_file_context();
+
+    ctx->options = options;
+    ctx->input_path = input_path;
+
+    sass_compile_file(ctx);
+    ret = output(ctx->error_status, ctx->error_message, ctx->output_string);
+
+    sass_free_file_context(ctx);
+    return ret;
+}
+
+int main(int argc, char** argv) {
+    int read_from_stdin = 0;
+    struct sass_options options;
+    options.output_style = SASS_STYLE_NESTED;
+    options.source_comments = 0;
+    options.image_path = "images";
+    options.include_paths = "";
+
+    int c;
+    while ((c = getopt(argc, argv, "lst:I:")) != -1) {
+        switch (c) {
+        case 'I':
+            options.include_paths = optarg;
+            break;
+        case 't':
+            if (strcmp(optarg, "compressed") == 0) {
+                options.output_style = SASS_STYLE_COMPRESSED;
+            } else if (strcmp(optarg, "compact") == 0) {
+                options.output_style = SASS_STYLE_COMPACT;
+            } else if (strcmp(optarg, "expanded") == 0) {
+                options.output_style = SASS_STYLE_EXPANDED;
+            } else if (strcmp(optarg, "nested") == 0) {
+                options.output_style = SASS_STYLE_NESTED;
+            } else {
+                fprintf(stderr, "Invalid argument for -t flag: '%s'\n", optarg);
+                /* No abort here, just use the default and continue */
+            }
+            break;
+        case 'l':
+            options.source_comments = 1;
+            break;
+        case 's':
+            read_from_stdin = 1;
+            break;
+        case '?':
+            /* Unrecognized flag or missing an expected value */
+            /* getopt should produce it's own error message for this case */
+            return 1;
+        default:
+            fprintf(stderr, "Unknown error while processing arguments\n");
+            return 2;
+        }
+    }
+
+    if (optind < argc && !read_from_stdin) {
+        return compile_file(options, argv[optind]);
+    } else {
+        return compile_stdin(options);
+    }
 }
