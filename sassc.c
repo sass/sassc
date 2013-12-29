35d0233fad8c3afb33339624dfc5e48ac376488e
Stijn Van Nieuwenhuyse <stijn@vannieuwenhuyse.eu>
2013-12-29 14:36:24 +0100

    Change input/output behavior to be similar to sass (Ruby).
    - When no parameters are given, the input is read from stdin and the
      output is printed to stdout.
    - When 1 parameter is given, it is interpreted as input path and the
      output is printed to stdout.
    - When 2 parameters are given, the first one is used as input path and
      the second one is used as output path.
    - When more than 2 parameters are provided, an error message is printed.

--- sassc.c
+++ sassc.c
@@ -124,10 +124,10 @@ struct
 
 void print_usage(char* argv0) {
     int i;
-    printf("Usage: %s [OPTION]... [FILE]\n\n", argv0);
+    printf("Usage: %s [options] [INPUT] [OUTPUT]\n\n", argv0);
     printf("Options:\n");
-    printf("   -o OUTFILE              Write output to specified file.\n");
-    printf("   -t, --style NAME                 Output style. Can be:");
+    printf("   -s, --stdin             Read input from standard input instead of an input file.\n");
+    printf("   -t, --style NAME        Output style. Can be:");
     for(i = NUM_STYLE_OPTION_STRINGS - 1; i >= 0; i--) {
         printf(" %s", style_option_strings[i].style_string);
         printf(i == 0 ? ".\n" : ",");
@@ -147,6 +147,7 @@ void invalid_usage(char* argv0) {
 
 int main(int argc, char** argv) {
     char *outfile = 0;
+    int from_stdin = 0;
     struct sass_options options;
     options.output_style = SASS_STYLE_NESTED;
     options.source_comments = 0;
@@ -157,6 +158,7 @@ int main(int argc, char** argv) {
     int long_index = 0;
     static struct option long_options[] =
     {
+        { "stdin",         no_argument,       0, 's' },
         { "load-path",     required_argument, 0, 'I' },
         { "style",         required_argument, 0, 't' },
         { "line-numbers",  no_argument,       0, 'l' },
@@ -164,10 +166,10 @@ int main(int argc, char** argv) {
         { "sourcemap",     no_argument,       0, 'm' },
         { "help",          no_argument,       0, 'h' }
     };
-    while ((c = getopt_long_only(argc, argv, "ho:lmt:I:", long_options, &long_index)) != -1) {
+    while ((c = getopt_long_only(argc, argv, "hslmt:I:", long_options, &long_index)) != -1) {
         switch (c) {
-        case 'o':
-            outfile = optarg;
+        case 's':
+            from_stdin = 1;
             break;
         case 'I':
             options.include_paths = optarg;
@@ -207,14 +209,20 @@ int main(int argc, char** argv) {
         }
     }
 
-    if(optind < argc - 1) {
+    if(optind < argc - 2) {
         fprintf(stderr, "Error: Too many arguments.\n");
         invalid_usage(argv[0]);
     }
 
-    if(optind < argc && strcmp(argv[optind], "-") != 0) {
+    if(optind < argc && strcmp(argv[optind], "-") != 0 && !from_stdin) {
+        if (optind + 1 < argc) {
+            outfile = argv[optind + 1];
+        }
         return compile_file(options, argv[optind], outfile);
     } else {
+        if (optind < argc) {
+            outfile = argv[optind];
+        }
         return compile_stdin(options, outfile);
     }
 }
