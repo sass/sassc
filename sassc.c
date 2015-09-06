sass/sassc

commit 369a70df9bb8285acca6061f27c98df871502b7d

Update for the new 3.3 header layout

sassc.c
@@ -8,9 +8,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <getopt.h>
-#include <sass2scss.h>
-#include <sass_context.h>
-#include "sassc_version.h"
+#include <sass.h>
 
 #define BUFSIZE 512
 #ifdef _WIN32

commit 4dff0b6b8eaedcf0a3a5e5a217e2a032942a752a

Fix build broken by 369a70df9bb8285acca6061f27c98df871502b7d

configure.ac
@@ -71,9 +71,10 @@ AC_CHECK_HEADERS([sass.h],, [AC_MSG_ERROR([unable to find libsass headers (use -
 
 # also check for additional libsass headers (just in case)
 AC_CHECK_HEADERS([sass2scss.h],, [AC_MSG_ERROR([unable to find sass2scss.h])])
-AC_CHECK_HEADERS([sass_values.h],, [AC_MSG_ERROR([unable to find sass_values.h])])
-AC_CHECK_HEADERS([sass_context.h],, [AC_MSG_ERROR([unable to find sass_context.h])])
-AC_CHECK_HEADERS([sass_functions.h],, [AC_MSG_ERROR([unable to find sass_functions.h])])
+AC_CHECK_HEADERS([sass/values.h],, [AC_MSG_ERROR([unable to find sass/values.h])])
+AC_CHECK_HEADERS([sass/functions.h],, [AC_MSG_ERROR([unable to find sass_functions.h])])
+AC_CHECK_HEADERS([sass/context.h],, [AC_MSG_ERROR([unable to find sass/context.h])], [
+#include <sass/functions.h>])
 
 AC_ARG_ENABLE([coverage],
   [AS_HELP_STRING([--enable-coverage],

sassc.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <sass.h>
+#include "sassc_version.h"
 
 #define BUFSIZE 512
 #ifdef _WIN32