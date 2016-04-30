77ad39483ef0976f7373752deb947776eeef75f8
Marcel Greter <marcel.greter@ocbnet.ch>
2016-04-30 09:28:03 +0200

    Avoid MSVC compiler warnings for C4996
    > The POSIX name for this item is deprecated.
    > Instead, use the ISO C and C++ conformant name
    https://msdn.microsoft.com/en-us/library/ttcz0bys.aspx

--- sassc.c
+++ sassc.c
@@ -1,7 +1,8 @@
 #ifdef _MSC_VER
+#ifndef _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS 1
 #endif
-#ifndef _CRT_SECURE_NO_WARNINGS
+#ifndef _CRT_NONSTDC_NO_WARNINGS
 #define _CRT_NONSTDC_NO_WARNINGS 1
 #endif
 #endif
